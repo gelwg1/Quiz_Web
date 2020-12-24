55a7b8030f467303aabf1ac20ec870c2184773f9
gelwg1 <gelwg1@gmail.com>
2020-12-24 14:14:21 +0700

    ADD more DATABASE

--- Signup/connection.php
+++ Signup/connection.php
@@ -1,8 +1,10 @@
 <?php
-$db_host = "localhost:3306";
-$db_user = "root";
-$db_password = "";
-$db_name = "Registration";
+
+
+$db_host = "103.124.95.89:3306";
+$db_user = "hust_web_121223";
+$db_password = "0ROk5ttMxO8U2WSu";
+$db_name = "hust_web_009";
 try {
 
     $db = new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_password);
